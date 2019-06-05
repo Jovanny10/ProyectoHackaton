d2a7172efe372b22555e592394d35ac791da6cdc
Jovanny Leobardo Salas <jovannyleobardo10@gmail.com>
2019-06-04 20:58:28 -0500

    Agregando estilo al lider del proyecto

--- Administrador/view/lider.php
+++ Administrador/view/lider.php
@@ -24,7 +24,7 @@
 			</table>
 		</div>
 <!--==========================================MODAL DETALLES================================================-->
-		<div class="modal fade" id="detalleslider">
+		<div class="modal fade" id="detalles">
 			<div class="modal-dialog" role="document">
 				<div class="modal-content">
 					<form method="POST">
