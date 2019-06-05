162d11f231a91e8b6ce5e5daf7797bf1492fe5d8
Jovanny Leobardo Salas <jovannyleobardo10@gmail.com>
2019-06-04 21:20:18 -0500

    Interfaz del hacker en la ventana modal editar del seccion administrador

--- Administrador/view/hackers.php
+++ Administrador/view/hackers.php
@@ -86,7 +86,7 @@
 <!--==========================================FIN MODAL DETALLES================================================-->
 <!--==========================================MODAL EDITAR================================================-->
 		<div class="modal fade" id="editar">
-			<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-dialog" role="document">
 				<div class="modal-content">
 					<form method="POST">
 						<div class="modal-header">
@@ -98,95 +98,22 @@
 						<div class="modal-body">
 							<input type="hidden" class="form-control" id="id" name="id" value="">
 							<div class="row">
-								<div class="col-lg-4">
-									<label for="#">Nombre</label>
-									<input type="text" class="form-control" name="nombre2" id="nombre2" onpaste= "alert('No puedes pegar');return false">
-								</div>
-								<div class="col-lg-4">
-									<label for="#">Apellido paterno</label>
-									<input type="text" class="form-control" name="App" id="App">
-								</div>
-								<div class="col-lg-4">
-									<label for="#">Apellido materno</label>
-									<input type="text" class="form-control" name="Apm" id="Apm">
+								<div class="col-lg-12">
+									<label for="#">Correo</label>
+									<input type="text" class="form-control" name="correo" id="correo" onpaste= "alert('No puedes pegar');return false">
 								</div>
 							</div>
 							<div class="row">
-								<div class="col-lg-4">
-									<label for="#">E-mail</label>
-									<input type="text" class="form-control" name="email1" id="email1" onpaste="alert('No puedes pegar');return false">
+								<div class="col-lg-12">
+									<label for="#">Contraseña</label>
+									<input type="text" class="form-control" name="pwd" id="pwd" onpaste="alert('No puedes pegar');return false">
 								</div>
-								<div class="col-lg-4">
+							</div>
+							<div class="row">
+								<div class="col-lg-12">
 									<label for="#">Celular</label>
-									<input type="text" class="form-control" name="telefono1" id="telefono1" onpaste="alert('No puedes pegar');return false">
-								</div>
-								<div class="col-lg-4">
-									<label for="#">F-Nacimiento</label>
-									<input type="text" class="form-control" name="fecha1" id="fecha1">
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-lg-4">
-									<label for="#">Institución</label>
-									<select id="institucion" class="form-control" name="institucion">
-									</select>
-								</div>
-								<div class="col-lg-4">
-									<label for="#">Carrera</label>
-									<select id="carrera" class="form-control" name="carrera">
-										
-									</select>
-								</div>
-								<div class="col-lg-4">
-									<label for="#">Sexo</label>
-									<select id="sexo" name="sexo" class="form-control">
-										
-									</select>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-lg-4">
-									<label for="#">Facebook</label>
-									<input type="text" class="form-control" name="facebook" id="facebook" onpaste="alert('No puedes pegar');return false" onkeypress="return sololetras(event)">
-								</div>
-								<div class="col-lg-4">
-									<label for="#">Twitter</label>
-									<input type="text" class="form-control" name="twitter1" id="twitter1" onpaste = "alert('No puedes pegar');return false">
-								</div>
-								<div class="col-lg-4">
-									<label for="#">Habilidad</label>
-									<input type="text" class="form-control" name="habilidad1" id="habilidad1">
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-lg-4">
-									<label for="#">Hobbie</label>
-									<input type="text" class="form-control" name="hobbie1" id="hobbie1" onpaste="alert('No puedes pegar');return false" onkeypress="return sololetras(event)">
-								</div>
-								<div class="col-lg-4">
-									<label for="#">Playera</label>
-									<select id="playera" name="playera" class="form-control">
-										<option selected="" value="0"> -- Seleccionar -- </option>
-									</select>
-								</div>
-								<div class="col-lg-4">
-									<label for="#">Rol</label>
-									<select id="rol" name="rol" class="form-control">
-										<option selected="" value="0"> -- Seleccionar -- </option>
-									</select>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-lg-4">
-									<label for="#">Psw</label>
-									<input type="text" class="form-control" name="psw1" id="psw1">
-								</div>
-								<div class="row justify-content-center">
-									<div class="col-mg-5">
-										<div class="informacion">
-											
-										</div>
-									</div>
+									<input type="text" id="celular" class="form-control" name="celular">
+									</input>
 								</div>
 							</div>
 						</div>
